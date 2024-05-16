3b52edd2d58dd8e1e6842f9cfc859cb647032d75
fuzoh <info@bastiennicoud.ch>
2024-05-16 12:28:04 +0200

    feat: scaffold ratatui

---
+++ README.md
@@ -0,0 +1,4 @@
+# Monitors
+
+> An ultra lignt, ultra simple binary to select a monitor configuration for hyprland.
+> Execure it on monitor plug/unplug
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, Result};
+
+use crossterm::{
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{prelude::CrosstermBackend, Terminal};
+
+fn main() -> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    // Implement program here
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
