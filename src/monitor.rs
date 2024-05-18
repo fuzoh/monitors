2cbc3d0fb1c6d2e1c46ded112beeb1e5f2be6fdb
fuzoh <info@bastiennicoud.ch>
2024-05-18 10:00:46 +0200

    feat: dto structure to represent hyprctl monitors output

---
+++ src/monitor.rs
@@ -0,0 +1,22 @@
+use serde::Deserialize;
+
+/// This structure represent data returned by the `hyprctl -j monitors all` command
+/// Only the useful informations are deserialised.
+/// See the command output for more informations.
+/// Compatible with hyprland 0.4.0
+#[derive(Deserialize)]
+pub struct Monitor {
+    id: u32,
+    name: String,
+    description: String,
+    width: u32,
+    height: u32,
+    scale: f32,
+    availableModes: Vec<Modes>,
+}
+
+pub struct Modes {
+    width: u32,
+    height: u32,
+    refreshRate: f32,
+}
