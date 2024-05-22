8b8002b579d2b240404c34566db89b7f1adedff7
fuzoh <info@bastiennicoud.ch>
2024-05-22 18:28:06 +0200

    feat: custom deserializer for monitor modes

--- src/monitor.rs
+++ src/monitor.rs
@@ -1,10 +1,11 @@
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer, Serialize};
 
 /// This structure represent data returned by the `hyprctl -j monitors all` command
 /// Only the useful informations are deserialised.
 /// See the command output for more informations.
 /// Compatible with hyprland 0.4.0
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Monitor {
     id: u32,
     name: String,
@@ -12,13 +13,36 @@ pub struct Monitor {
     width: u32,
     height: u32,
     scale: f32,
-    #[serde(rename = "availableModes")]
     available_modes: Vec<Modes>,
 }
 
+#[derive(PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Modes {
     width: u32,
     height: u32,
-    #[serde(rename = "refreshRate")]
     refresh_rate: f32,
+}
+
+/// Custom deserialisation to transform monitor resolution string into Modes struct
+/// Input "3840x2400@60.00Hz"
+impl<'de> Deserialize<'de> for Modes {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s: String = Deserialize::deserialize(deserializer)?;
+        let mut parts = s.split('@');
+        let resolution = parts.next().unwrap();
+        let refresh_rate = parts.next().unwrap();
+        let mut resolution_parts = resolution.split('x');
+        let width: u32 = resolution_parts.next().unwrap().parse().unwrap();
+        let height: u32 = resolution_parts.next().unwrap().parse().unwrap();
+        let refresh_rate: f32 = refresh_rate.trim_end_matches("Hz").parse().unwrap();
+        Ok(Modes {
+            width,
+            height,
+            refresh_rate,
+        })
+    }
 }
