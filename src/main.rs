74be9c54d6fe6caeb24518606bd2ee117117d9bc
fuzoh <info@bastiennicoud.ch>
2024-05-30 07:54:31 +0200

    refactor: move ratatui app logig in dedicated file

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,27 @@
 use std::io;
+use std::process::Command;
 
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{
-    prelude::*,
-    symbols::border,
-    widgets::{block::*, *},
-};
-
+use std::string::String;
 mod tui;
+mod app;
+mod monitor;
+mod actions;
 
-#[derive(Debug, Default)]
-pub struct App {
-    counter: u8,
-    exit: bool,
-}
+use monitor::Monitor;
+use app::App;
 
-impl App {
-    pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.render_frame(frame))?;
-            self.handle_events()?;
-        }
-        Ok(())
-    }
-
-    fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
-        Ok(())
-    }
-
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement_counter(),
-            KeyCode::Right => self.increment_counter(),
-            _ => {}
-        }
-    }
-
-    fn exit(&mut self) {
-        self.exit = true;
-    }
-
-    fn increment_counter(&mut self) {
-        self.counter += 1;
-    }
-
-    fn decrement_counter(&mut self) {
-        self.counter -= 1;
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" Counter App ".bold());
-        let instructions = Title::from(Line::from(vec![
-            " Decrement ".into(),
-            "<left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]));
-        let block = Block::default()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .borders(Borders::ALL)
-            .border_set(border::THICK);
-
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
-
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
-    }
-}
 
 fn main() -> io::Result<()> {
+    // Get the monitors form hyprctl
+    let hyprctl_output = Command::new("hyprctl")
+        .arg("-j")
+        .arg("monitors")
+        .arg("all")
+        .output()
+        .expect("Failed to get monitors from hyprctl");
+    let output_string = String::from_utf8(hyprctl_output.stdout).expect("Failed to parse the command output as utf8 string");
+    let monitors: Vec<Monitor> = serde_json::from_str(&output_string).expect("Failed to parse the command output as json");
+
     let mut terminal = tui::init()?;
     let app_result = App::default().run(&mut terminal);
     tui::restore()?;
